eacedc6db77111141b7ae2a7f765ecf205d11962
Hidde Bleeker <hiddebleeker2@gmail.com>
2017-06-19 19:49:44 +0200

    Added rho (damage prevention efficiency parameter)

--- globals.h
+++ globals.h
@@ -23,8 +23,10 @@ const double alpha1 = 4.0;             /// Displacement of curve over x-axis; da
 const double alpha2 = 4.0;             /// Displacement of curve over x-axis; damage2
 const double beta1 = 4.0;               /// Steepness of curve; damage1
 const double beta2 = 4.0;               /// Steepness of curve; damage2
-const double gamma1 = 0.25;              /// 'Multiplier' of the amount of damage added
+const double gamma1 = 0.4;              /// 'Multiplier' of the amount of damage added
 const double gamma2 = 0.4;              /// 'Multiplier' of the amount of damage added
+const double rho1 = 1.0;                /// Parameter for efficiency of damage prevention to trait 1
+const double rho2 = 0.5;                /// Parameter for efficiency of damage prevention to trait 2
 
 ///
 ///Parameters for setting Genes from normal distribution (genes vary from -inf to +inf)
--- individual.h
+++ individual.h
@@ -88,9 +88,11 @@ inline void Individual::calcResources() {
     fecundity = 1 - exp(-f_c * offspringAlloc);							// Max fecundity not above 1.0
     lifetimeRS += fecundity;
 
-	double repairTrait1 = 1 / (1 + exp(-genes[2] * (((damages[0] - damages[1]) / damTot) + genes[3])));
-	damages[0] += gamma1 * offspringAlloc * (1.0 - repairTrait1);   // repair allocation = 1 - offspring; thus "damage allocation" = offspringAlloc
-	damages[1] += gamma2 * offspringAlloc * repairTrait1;           // baseDam added
+    double repairTrait1 = 1 / (1 + exp(-genes[2] * (((damages[0] - damages[1]) / damTot) + genes[3])));
+    double repairTrait2 = 1.0 - repairTrait1;
+
+	damages[0] += gamma1 * offspringAlloc * (1.0 - rho1 * repairTrait1);    // repair allocation = 1 - offspring; thus "damage allocation" = offspringAlloc
+	damages[1] += gamma2 * offspringAlloc * (1.0 - rho2 * repairTrait2);    // baseDam added
 	for (auto& dam : damages) if (dam > 1.0) dam = 1.0;				// Make sure damage is not above 1.0
 };
 
